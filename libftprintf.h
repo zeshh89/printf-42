61aa59d7261aa607aceee3f81da1592c9116cdb8
Jose Antonio Garcia Ruiz <jose-an2@car6s3.42barcelona.com>
2025-10-13 20:36:45 +0200

    first push

---
+++ libftprintf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-an2 <jose-an2@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 17:41:54 by jose-an2          #+#    #+#             */
+/*   Updated: 2025/10/13 17:58:14 by jose-an2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+//function declarations
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstring_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+int		ft_printf(char const *str, ...);
+
+#endif
